1da8557b3cfe950c87f275a7ad01b4b1550c97d3
Gabriel Cruz <gabrielcruz31@gmail.com>
2022-02-18 02:32:36 -0300

    Adicionado link para registrar usuário.

--- templates/Users/login.php
+++ templates/Users/login.php
@@ -8,6 +8,7 @@
     <?= $this->Form->create() ?>
     <fieldset>
         <legend><?= __('Entre com seu usuário e senha para fazer o login') ?></legend>
+        <legend><?= $this->Html->link(__('Registrar usuário'), ['controller' => 'Users', 'action' => 'add']) ?></legend>
         <?= $this->Form->control('username', ['label' => 'Usuário', 'required' => true]) ?>
         <?= $this->Form->control('password', ['label' => 'Senha', 'required' => true]) ?>
     </fieldset>
